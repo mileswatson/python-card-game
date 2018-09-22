b2e8e2236caa2da387e41167128b9fb418c65f62
Miles Watson <milestwatson@icloud.com>
2018-09-22 12:45:05 +0100

    Subwindows now preload, and are shown/hidden rather than created+destroyed.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,68 @@
-import appJar as aj
+import appJar
 import sqlite3 as sql
+from hashlib import sha256
 
-app = aj.gui("Cardography")
+app = appJar.gui("Cardography")
 directory = ""
 db = None
+player1 = "1"
+player2 = "2"
 
-class UserLogin:
+def dummy(btn=None):
+    ChooseGame.stop()
+
+class ChooseAuthentication:
+
+    btn = ""
 
     @staticmethod
-    def go(path):
+    def preload():
+        app.startSubWindow("ChooseAuthentication", modal=True)
+        app.addNamedButton("Login", "ChooseAuthentication.Login", ChooseAuthentication.back)
+        app.addNamedButton("Cancel","ChooseAuthentication.Cancel", ChooseAuthentication.back)
+        app.setStopFunction(ChooseAuthentication.back)
+        app.stopSubWindow()
+
+    @staticmethod
+    def go(btn=None):
+        ChooseAuthentication.btn = btn
+        app.showSubWindow("ChooseAuthentication")
+    
+    @staticmethod
+    def resume(btn=None):
+        pass
+    
+    @staticmethod
+    def back(btn=None):
+        app.hideSubWindow("ChooseAuthentication")
+        if btn == "ChooseAuthentication.Login":
+            SelectPlayers.resume(ChooseAuthentication.btn)
+        else:
+            SelectPlayers.resume(None)
+
+
+class SelectPlayers:
+
+    @staticmethod
+    def preload():
+        app.startSubWindow("Select Players")
+        app.addNamedButton("", "SelectPlayers.Player1", ChooseAuthentication.go)
+        app.addLabel("SelectPlayers.Username1", "   Username: ")
+        app.addLabel("SelectPlayers.HighScore1", "   High Score: ")
+        app.addEmptyLabel("SelectPlayers.Empty1")
+        app.addNamedButton("", "SelectPlayers.Player2", ChooseAuthentication.go)
+        app.addLabel("SelectPlayers.Username2", "   Username: ")
+        app.addLabel("SelectPlayers.HighScore2", "   High Score: ")
+        app.addEmptyLabel("SelectPlayers.Empty2")
+        app.addButton("Back", SelectPlayers.back)
+        app.setStopFunction(SelectPlayers.back)
+        app.stopSubWindow()
+
+    @staticmethod
+    def go():
         global db
-        db = sql.connect(path+"/"+"users.dbf")
+        db = sql.connect(directory+"/"+"users.dbf")
         cursor = db.cursor()
-        
         cursor.execute('''
             SELECT name FROM sqlite_master
             WHERE type="table"
@@ -24,47 +74,62 @@ class UserLogin:
                     id          INTEGER PRIMARY KEY,
                     username    TEXT,
                     password    CHAR(32),
-                    best        INTEGER
+                    best        INTEGER,
+                    used        INTEGER
                 )
             ''')
             db.commit()
-        
-        app.startSubWindow("Login")
-        app.addLabelEntry("Username: ")
-        app.addSecretLabelEntry("Password: ")
-        app.stopSubWindow()
-        app.showSubWindow("Login")
+        app.setButton("SelectPlayers.Player1", "")
+        app.setButton("SelectPlayers.Player2", "")
+        app.showSubWindow("Select Players")
+    
+    @staticmethod
+    def resume(btn=None):
+        if btn != None:
+            app.setButton(btn, player1 if btn[-1] == "1" else player2)
+            
+    
+    @staticmethod
+    def back(btn=None):
+        db.commit()
+        db.close()
+        app.hideSubWindow("Select Players", useStopFunction=False)
+        ChooseGame.resume()
 
 
 
 class ChooseGame:
 
     @staticmethod
-    def go():
-        app.startSubWindow("Choose")
-        app.addButton("Select Folder", ChooseGame.choose)
-        app.addButton("Exit", ChooseGame.stop)
+    def preload():
+        app.startSubWindow("Choose Game")
+        app.addNamedButton("Select Folder", "ChooseGame.SelectFolder", ChooseGame.selectFolder)
+        app.addNamedButton("Exit", "ChooseGame.Exit", ChooseGame.stop)
         app.setStopFunction(ChooseGame.stop)
         app.stopSubWindow()
-        app.go(startWindow = "Choose")
 
     @staticmethod
-    def choose(btn=None):
-        global directory
-        directory = app.directoryBox(
-            title="Select Folder"
-        )
-        if directory != None:
-            app.hideSubWindow("Choose")
-            UserLogin.go(directory)
+    def go():
+        app.go(startWindow = "Choose Game")
 
     @staticmethod
     def resume(btn=None):
-        app.showSubWindow("Choose")
+        app.showSubWindow("Choose Game")
 
     @staticmethod
     def stop(btn=None):
-        print("here")
         app.stop()
+    
+    @staticmethod
+    def selectFolder(btn=None):
+        global directory
+        directory = app.directoryBox(title="Select Folder")
+        if directory != None:
+            app.hideSubWindow("Choose Game")
+            SelectPlayers.go()
+
+ChooseGame.preload()
+SelectPlayers.preload()
+ChooseAuthentication.preload()
 
 ChooseGame.go()
